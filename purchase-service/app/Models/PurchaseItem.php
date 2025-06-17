21911614cfc8225a392faad96847d7aff9c57e36
19yoiram <iramsahar7135@gmail.com>
2025-06-17 17:53:15 +0500

    microservices

--- purchase-service/app/Models/PurchaseItem.php
+++ purchase-service/app/Models/PurchaseItem.php
@@ -10,7 +10,7 @@ class PurchaseItem extends Model
 {
  protected $fillable = [
     'purchase_id', 'product_id', 'quantity', 'purchase_price',
-    'discount', 'tax', 'tax_amount', 'unit_cost', 'total_cost'
+    'discount', 'tax_percent', 'tax_amount', 'unit_cost', 'total_cost'
 ];
 
     public function purchase()
